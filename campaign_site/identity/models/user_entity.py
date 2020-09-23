e095c66fb8501e25c5e44947261d2a6a32ae3270
Marjan97 <marjanazizi1392@gmail.com>
2020-09-23 12:04:02 -0700

    add manager

--- campaign_site/identity/models/user_entity.py
+++ campaign_site/identity/models/user_entity.py
@@ -8,10 +8,10 @@ from commons.models.base_entity import BaseEntity
 
 class UserEntity(AbstractUser, BaseEntity):
     username = None
-    mobile = models.CharField(max_length=11, unique=True)
+    email = models.EmailField(_('email address'), blank=False, unique=True)
+    mobile = models.CharField(max_length=11,blank=True, help_text='Contact phone number',unique=True)
     otp = models.PositiveIntegerField(blank=True, null=True)
     otp_create_time = models.DateTimeField(auto_now=True)
-
 
     USERNAME_FIELD = 'mobile'
     objects = MyUserManager()
