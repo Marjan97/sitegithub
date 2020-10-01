d407c7795acd943834a3c5ef0fe7d5b04175102e
maedeh <ma.shahabi15@gmail.com>
2020-10-01 12:22:41 +0330

    user_type added to user_enmtity model.

---
+++ campaign_site/identity/enums.py
@@ -0,0 +1,6 @@
+from enum import Enum
+
+class UserType(Enum):
+    superadmin = 1
+    admin = 2
+    simple = 3
--- campaign_site/identity/models/user_entity.py
+++ campaign_site/identity/models/user_entity.py
@@ -2,6 +2,7 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 from django.utils.translation import gettext_lazy as _
 # from phone_field import PhoneField
+from identity.enums import UserType
 from identity.manager import MyUserManager
 from commons.models.base_entity import BaseEntity
 
@@ -9,11 +10,12 @@ from commons.models.base_entity import BaseEntity
 class UserEntity(AbstractUser, BaseEntity):
     username = None
     email = models.EmailField(_('email address'), blank=False, unique=True)
-    mobile = models.CharField(max_length=11,blank=True, help_text='Contact phone number',unique=True)
+    mobile_phone_number = models.CharField(max_length=11, blank=True, help_text='Contact phone number', unique=True)
     otp = models.PositiveIntegerField(blank=True, null=True)
     otp_create_time = models.DateTimeField(auto_now=True)
+    user_type = models.IntegerField(choices=UserType.choices(), default=UserType.simple.value)
 
-    USERNAME_FIELD = 'mobile'
     objects = MyUserManager()
 
-    REQUIRED_FIELDS = []
+    REQUIRED_FIELDS = [mobile_phone_number]
+    USERNAME_FIELD = mobile_phone_number
